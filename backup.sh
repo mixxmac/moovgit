d98f0dc3b57a7589909fc3e256e015393921bc71
Ted Hanson <ted.hanson@moovweb.com>
2013-04-17 13:16:02 -0700

    Fix code to use wkhtmltopdf

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ DIRTO="html-log"
 getURI() {
   PATH1="./$DIRTO/${2:2}"
   URL="${1:5}"
-  [ "${1:0:5}" == '#URI=' ] && echo "Backing up $2" && wget -O "$PATH1.pdf" "pdfmyurl.com?url=${URL/www/mstage}" && curl "$URL" > "$PATH1.html"  
+  [ "${1:0:5}" == '#URI=' ] && echo "Backing up $2" && curl "$URL" > "$PATH1.html" && ./wkhtmltopdf -l ${URL/www/mstage} "$PATH1.pdf"   
 }
 
 backup() {
